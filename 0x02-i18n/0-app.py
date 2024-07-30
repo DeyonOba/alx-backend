22485779d7f71f19766813832c263d431f53fe48
Gideon Oba <deyonoba@gmail.com>
2024-07-30 14:50:36 +0100

    refactor: 0-app.py pass pycodestyle linter

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     """Set up a single / route."""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
